81a2660a0726074b7809d9f5410e5d10115df3ac
username <phil042017@gmail.com>
2022-11-01 17:59:57 -0400

    Update to array exercise

--- C Functions/main.c
+++ C Functions/main.c
@@ -9,8 +9,16 @@
 
 void scoreReturn(double scores[], int scoreCount);
 
-int main(int argc, const char * argv[]) {
-    
+int main()
+{
+    int scoreCount = 5;
+    double scores[5];
+    scores[0]=50.00;
+    scores[1]=60.00;
+    scores[2]=40.00;
+    scores[3]=20.00;
+    scores[4]=10.00;
+    scoreReturn(scores, scoreCount);
 }
 
 void scoreReturn(double scores[], int scoreCount)
@@ -20,13 +28,10 @@ void scoreReturn(double scores[], int scoreCount)
     FILE * inPtr;
     inPtr = fopen("/Users/philsterr/Desktop/GitHub/C Funtions/C Functions/C Functions/bonus.txt", "r");
     fscanf(inPtr, "%lf", &bonusVar);
-    for(i=0; i<scoreCount-1; i++)
+    for(i=0; i< scoreCount; i++)
     {
-        scoreCount = scores[scoreCount] + bonusVar;
-    }
-    for(i=0; i<scoreCount-1; i++)
-    {
-        fprintf(inPtr, "\n%d", scoreCount);
+        scores[i] = scores[i] + bonusVar;
+        printf("%lf\n", scores[i]);
     }
     fclose(inPtr);
 }
